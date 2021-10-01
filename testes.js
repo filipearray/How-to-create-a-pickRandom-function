7987ee92abd2153e0a0befc31c5f26cca9400359
Filipe Guimarães <91645260+filipearray@users.noreply.github.com>
2021-10-01 11:59:10 -0300

    Add files via upload

---
+++ testes.js
@@ -0,0 +1,7 @@
+function pickRandom(warray){
+    var randomNumber = Math.floor(Math.random() * warray.length); 
+    return warray[randomNumber]; // warray = Words Array
+    }
+    var words = ['Mushoku Tensei', 'Kono Subarashii', 'Black Clover', 'We Never Learn', 'Go-toubun no Hanayome', 'Kaifuku Jutsushi', 'OreGairu', 'Kawaiikereba', 'Dragon Maid S', 'Tsuredere Children'];
+    var random = pickRandom(words);
+    console.log(random);
