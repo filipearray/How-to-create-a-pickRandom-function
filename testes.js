4f4ec90b784526aa0e7940d70b4acd7c80766b73
Filipe Guimarães <91645260+filipearray@users.noreply.github.com>
2022-01-19 16:27:27 -0300

    Update testes.js

--- testes.js
+++ testes.js
@@ -4,4 +4,4 @@ function pickRandom(warray){
     }
     var words = ['Mushoku Tensei', 'Kono Subarashii', 'Black Clover', 'We Never Learn', 'Go-toubun no Hanayome', 'Kaifuku Jutsushi', 'OreGairu', 'Kawaiikereba', 'Dragon Maid S', 'Tsuredere Children'];
     var random = pickRandom(words);
-    console.log(random);            
+    console.log(random);           
